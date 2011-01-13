733432bb05192a20593007e967a62916caa671e1
Joel Meador <joel@expectedbehavior.com>
2011-01-12 21:20:31 -0500

    state of app after my presentation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -16,23 +16,23 @@ jQuery(function($) {
   }
 
   $('tr a[data-method=delete]')
-//     .bind("ajax:error",      function(event, xhr, status, error) {
-//       flash(xhr.responseText)
-//     })
-    .bind("ajax:success",    function(event, data, status, xhr) {
-      $(this).closest('tr').remove()
-      //flash(data)
+    .bind("ajax:error",      function(event, xhr, status, error) {
+      flash(xhr.responseText)
     })
-//     .bind("ajax:beforeSend", function(event, xhr, settings) {
-//       $(this).parent().append('<img src="/images/ajax-loader.gif"/>')
-//     })
-//     .bind("ajax:complete", function(event, xhr, status) {
-//       $('#log').append($(this).closest('tr').attr('id').replace(/post_/, '')+'<br/>');
-//     })
-
-//   $('form[data-remote=true]')
-//     .bind("ajax:success", function(event, data, status, xhr) {
-//       $(this).find(":input").val('')
+//     .bind("ajax:success",    function(event, data, status, xhr) {
+//       $(this).closest('tr').remove()
 //       flash(data)
 //     })
+    .bind("ajax:beforeSend", function(event, xhr, settings) {
+      $(this).parent().append('<img src="/images/ajax-loader.gif"/>')
+    })
+    .bind("ajax:complete", function(event, xhr, status) {
+      $('#log').append($(this).closest('tr').attr('id').replace(/post_/, '')+'<br/>');
+    })
+
+  $('form[data-remote=true]')
+    .bind("ajax:success", function(event, data, status, xhr) {
+      $(this).find(":input").val('')
+      flash(data)
+    })
 })
